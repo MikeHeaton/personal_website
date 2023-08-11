a5285d3ddd8d3311485aefa4686cf934fd57d7bc
Mike Heaton <mike@openai.com>
2023-08-11 10:56:49 +0100

    better styling

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import Head from 'next/head';
-import Link from 'next/link';
-import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
@@ -12,92 +10,86 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className={styles.title}>
-          Hi, I'm Mike.
-        </h1>
+        <div className="layoutContainer">
+          <div className="titleContainer">
+            <h1>
+              Hi, I'm Mike.
+            </h1>
+          </div>
 
-        <div className="ppContainer">
-        <Image
-          src="/face.png"
-          width={500}
-          height={500}
-          alt="Picture of the author"
-        />
-        </div>
+          <div className="profileContainer">
+            <img 
+                className="profileimage"
+                src="/face.png"
+                alt="Face"
+            ></img>
+          </div>  
 
-        <div className="contactsContainer">
-          <a href="https://www.linkedin.com/in/mikeheatonsf/" className="contact"><Image
-            src="/linkedin-circle-large.png"
-            width={50}
-            height={50}
-            alt="Linkedin: @mikeheatonsf"
-          /></a>
+          <div className="contactsContainer">
+            <a href="https://www.linkedin.com/in/mikeheatonsf/" className="contactContainer">
+              <img 
+                src="/linkedin-circle-large.png"
+                alt="Linkedin: @mikeheatonsf"
+              ></img>
+            </a>
 
-          <a href="mailto:mike@mikeheaton.uk=" className="contact"><Image
-            src="/email-circle-large.png"
-            width={50}
-            height={50}
-            alt="Email: mike@mikeheaton.uk"
-          /></a>
+            <a href="mailto:mike@mikeheaton.uk" className="contactContainer">
+              <img 
+                src="/email-circle-large.png"
+                alt="Email: mike@mikeheaton.uk"
+              ></img>
+            </a>
 
-          <a href="https://twitter.com/realmikeheaton/" className="contact"><Image
-            src="/twitter-circle-large.png"
-            width={50}
-            height={50}
-            alt="Twitter: @realmikeheaton"
-          /></a>
+            <a href="https://twitter.com/realmikeheaton/" className="contactContainer">
+              <img 
+                src="/twitter-circle-large.png"
+                alt="Twitter: @realmikeheaton"
+              ></img>
+            </a>
+          </div>
         </div>
       </main>
 
       <style jsx>{`
-        main {
-          padding: 5%;
-          flex: 1;
+        .layoutContainer {
+          width: 100vw;
+          max-width: 100vw;
           display: flex;
           flex-direction: column;
           justify-content: center;
-          align-items: center;
         }
-        footer {
+        .titleContainer {
           width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
           display: flex;
           justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+          > h1 {
+            font-size: 8vw;
+            line-height: 10vw;
+          }
         }
         .contactsContainer {
           display: flex;
-          flex-wrap: wrap;
-          justify-content: space-between;
-          width:80%;
-          padding: 5%;
+          justify-content: space-around;
+          width: 100%;
+          padding: 5%; 
         }
-        .contact {
-          width: 20%;
+        .contactContainer {
+          max-width: 15%; 
         }
-        .ppContainer {
+        .contactContainer img {
+          max-width: 100%;
+          max-height: 100%;
+        }
+        .profileContainer {
+          width: 100%;
+          display: flex;
           justify-content: center;
-          padding: 5%;
         }
+        .profileContainer img {
+          padding: 15%;
+          max-width: 100%;
+          max-height: 100%;
+        }        
       `}</style>
 
       <style jsx global>{`
